b49e2a8e95b30eaa414fed0d89deeaa34757008a
hashim1sharif <haashimsh@hotmail.com>
2025-11-16 15:47:17 +0100

    up te date

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Health check endpoint (for ALB target group)
 app.get('/api/healthz', (req, res) => {
-  res.status(200).json({ message: 'server is healthy' });
+  res.status(200).json({ message: 'If you see this Hashim, the server is healthy' });
 });
 
 // Catch-all for SPA routes (React, Vue, etc.)
